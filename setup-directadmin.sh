e9a61773e37e1e8da574d14b4749dc03f044ef9a
realcryptonight2 <me@realcryptonight.dev>
2022-03-24 14:07:04 +0100

    Changed: Added support for the new install script

--- setup-directadmin.sh
+++ setup-directadmin.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
+if [ -z "$1" ]
+  then
+    echo "No license key given."
+	exit 0;
+fi
+
 # First install and configure the server as a normal install.
 chmod 777 ./standard-settings.sh
 ./standard-settings.sh
 
-# Pre-Install DirectAdmin packages.
-apt -y install sshpass gcc g++ make flex bison openssl libssl-dev perl perl-base perl-modules libperl-dev libperl4-corelibs-perl libwww-perl libaio1 libaio-dev zlib1g zlib1g-dev libcap-dev cron bzip2 zip automake autoconf libtool cmake pkg-config python libdb-dev libsasl2-dev libncurses5 libncurses5-dev libsystemd-dev bind9 dnsutils quota patch logrotate rsyslog libc6-dev libexpat1-dev libcrypt-openssl-rsa-perl libnuma-dev libnuma1
+# Run Common pre-install commands
+apt -y update
+apt -y upgrade
+apt -y install gcc g++ make flex bison openssl libssl-dev perl perl-base perl-modules libperl-dev libperl4-corelibs-perl libwww-perl libaio1 libaio-dev zlib1g zlib1g-dev libcap-dev cron bzip2 zip automake autoconf libtool cmake pkg-config python libdb-dev libsasl2-dev libncurses5 libncurses5-dev libsystemd-dev bind9 dnsutils quota patch logrotate rsyslog libc6-dev libexpat1-dev libcrypt-openssl-rsa-perl libnuma-dev libnuma1
+
+# Collect some required info.
+serverip=`hostname -I | awk '{print $1}'`
+serverhostname=`dig -x ${serverip} +short | sed 's/\.[^.]*$//'`
+domainhostname=`echo $serverhostname | sed 's/^[^.]*.//g'`
+ns1host="ns1.${domainhostname}"
+ns2host="ns2.${domainhostname}"
+
+# Set some variables to let DirectAdmin install correctly.
+export DA_CHANNEL=current
+export DA_HOSTNAME=$serverhostname
+export DA_NS1=$ns1host
+export DA_NS2=$ns2host
+export DA_FOREGROUND_CUSTOMBUILD=yes
 
 # Download and run the DirectAdmin install script.
-wget -O install.sh https://www.directadmin.com/setup.sh
+wget -O install.sh https://download.directadmin.com/setup.sh
 chmod 755 install.sh
-./install.sh auto
+./install.sh $1
 
 # Enable and build cURL in CustomBuilds.
 cd /usr/local/directadmin/custombuild
@@ -32,11 +54,10 @@ cd /usr/local/directadmin/custombuild
 ./build rewrite_confs
 ./build update
 ./build letsencrypt
-. /usr/local/directadmin/scripts/setup.txt
 
 # Request LetsEncrypt Certificates for the directadmin domain itself.
-/usr/local/directadmin/scripts/letsencrypt.sh request_single $hostname 4096
-/usr/local/directadmin/directadmin set ssl_redirect_host $hostname
+/usr/local/directadmin/scripts/letsencrypt.sh request_single $serverhostname 4096
+/usr/local/directadmin/directadmin set ssl_redirect_host $serverhostname
 service directadmin restart
 
 # Enable multi SSL support for the mail server.
@@ -46,8 +67,6 @@ cd /usr/local/directadmin/custombuild
 ./build clean
 ./build update
 ./build set eximconf yes
-# Commented out since 4.5 should be standard and newer versions should be allowed.
-#./build set eximconf_release 4.5
 ./build set dovecot_conf yes
 ./build exim_conf
 ./build dovecot_conf
@@ -57,5 +76,5 @@ clear
 . /usr/local/directadmin/scripts/setup.txt
 echo "Username: $adminname"
 echo "Password: $adminpass"
-echo "Domain: $hostname"
-echo "Please reboot the server to make sure IPv6 is enabled."
+echo "Domain: $serverhostname"
+
