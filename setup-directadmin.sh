e8fb5d29da883fc1fa46303a5ea9955b177783af
realcryptonight2 <me@realcryptonight.dev>
2022-05-12 10:25:20 +0200

    Readded: SFTP install and moved things around

--- setup-directadmin.sh
+++ setup-directadmin.sh
@@ -14,7 +14,7 @@ chmod 755 setup-standard.sh
 # Run common pre-install commands
 apt -y update
 apt -y upgrade
-apt -y install gcc g++ make flex bison openssl libssl-dev perl perl-base perl-modules libperl-dev libperl4-corelibs-perl libwww-perl libaio1 libaio-dev zlib1g zlib1g-dev libcap-dev cron bzip2 zip automake autoconf libtool cmake pkg-config python libdb-dev libsasl2-dev libncurses5 libncurses5-dev libsystemd-dev bind9 dnsutils quota patch logrotate rsyslog libc6-dev libexpat1-dev libcrypt-openssl-rsa-perl libnuma-dev libnuma1
+apt -y install sshpass gcc g++ make flex bison openssl libssl-dev perl perl-base perl-modules libperl-dev libperl4-corelibs-perl libwww-perl libaio1 libaio-dev zlib1g zlib1g-dev libcap-dev cron bzip2 zip automake autoconf libtool cmake pkg-config python libdb-dev libsasl2-dev libncurses5 libncurses5-dev libsystemd-dev bind9 dnsutils quota patch logrotate rsyslog libc6-dev libexpat1-dev libcrypt-openssl-rsa-perl libnuma-dev libnuma1
 
 # Get the server IP for reverse DNS lookup.
 serverip=`hostname -I | awk '{print $1}'`
@@ -41,11 +41,6 @@ wget -O directadmin.sh https://download.directadmin.com/setup.sh
 chmod 755 directadmin.sh
 ./directadmin.sh $1
 
-# Enable and build cURL in CustomBuilds and build it.
-cd /usr/local/directadmin/custombuild
-sed -i "s/curl=no/curl=yes/g" options.conf
-./build curl
-
 # Request LetsEncrypt Certificates for the directadmin domain itself.
 /usr/local/directadmin/scripts/letsencrypt.sh request_single $serverhostname 4096
 systemctl restart directadmin.service
@@ -61,6 +56,33 @@ cd /usr/local/directadmin/custombuild
 ./build exim_conf
 ./build dovecot_conf
 echo "action=rewrite&value=mail_sni" >> /usr/local/directadmin/data/task.queue
+
+# Change the installed PHP versions.
+cd /usr/local/directadmin/custombuild
+./build update
+./build set php1_release 8.1
+./build set php2_release 8.0
+./build set php3_release 7.4
+./build set php1_mode php-fpm
+./build set php2_mode php-fpm
+./build set php3_mode php-fpm
+./build php n
+./build rewrite_confs
+
+# Download and install sftp scripts by poralix for ssh backup support.
+cd /usr/local/directadmin/scripts/custom/
+wget -O ssh_script.zip https://github.com/poralix/directadmin-sftp-backups/archive/refs/heads/master.zip
+unzip ssh_script.zip
+cd directadmin-sftp-backups-master/
+mv ftp_*.php ./../
+cd ..
+rm -rf directadmin-sftp-backups-master/
+rm ssh_script.zip
+
+# Enable and build cURL in CustomBuilds and build it.
+cd /usr/local/directadmin/custombuild
+sed -i "s/curl=no/curl=yes/g" options.conf
+./build curl
 
 # Clear the screen and display the login data.
 clear
